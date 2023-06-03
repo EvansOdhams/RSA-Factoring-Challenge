ac91aa10c5498da7117bcf0f242820e2bd24f256
EvansOdhams <evansodhams@gmail.com>
2023-06-03 12:32:36 +0300

    Add funtions and scripts Factorize as many numbers as possible into a product of two smaller numbers.

--- factors_functions.py
+++ factors_functions.py
@@ -1,52 +1,35 @@
 #!/usr/bin/python3
 import sys
-import time
-import math
+import ctypes
+from resource import getrusage as resource_usage, RUSAGE_SELF
+from time import time as timestamp
 
 
 def unix_time(function):
-    """
-    Return `real`, `sys`, and `user` elapsed time, UNIX 'time' command.
-    """
-    start_time = time.time()
+    '''Return `real`, `sys` and `user` elapsed time, like UNIX's command `time`
+    You can calculate the amount of used CPU-time used by your
+    function/callable by summing `user` and `sys`. `real` is just like the wall
+    clock.
+    Note that `sys` and `user`'s resolutions are limited by the resolution of
+    the operating system's software clock (check `man 7 time` for more
+    details).
+    '''
+    start_time, start_resources = timestamp(), resource_usage(RUSAGE_SELF)
     function()
-    end_time = time.time()
+    end_resources, end_time = resource_usage(RUSAGE_SELF), timestamp()
 
-    real_time = end_time - start_time
-    user_time = time.process_time()
-    sys_time = real_time - user_time
-
-    return f"\nreal: {real_time}\nuser: {user_time}\nsys: {sys_time}"
-
-
-def trial_division(n: int) -> int:
-    """
-    Finds the smallest divisor, if any, of a given number `n`
-    Returns:
-        smallest divisor if found
-        0 if n is prime
-    """
-    if n % 2 == 0:
-        print(f"{n}={n // 2}*2")
-        return 0
-
-    f = 3
-    while f * f <= n:
-        if n % f == 0:
-            print(f"{n}={n // f}*{f}")
-            return 0
-        f += 2
-
-    print(f"{n}={n}*1")
-    return 0
+    return "\nreal: {}\nuser: {}\nsys: {}".format(
+        end_time - start_time,
+        end_resources.ru_utime - start_resources.ru_utime,
+        end_resources.ru_stime - start_resources.ru_stime)
 
 
 def print_factors():
+    fun = ctypes.CDLL("./lib_factors_functions.so")
+    fun.trial_division.argtypes = [ctypes.c_long]
     with open(sys.argv[1], 'r') as prime:
-        for line in prime:
-            n = int(line.strip())
-            trial_division(n)
-
-
-if __name__ == '__main__':
-    print(unix_time(print_factors))
+        line = prime.readline()
+        while line != '':
+            n = int(line)
+            fun.trial_division(n)
+            line = prime.readline()
